0324e855dc1545c8297dbe8e6fd2fa05c330e81c
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-12-20 18:38:45 +0530

    Adding method to calculate the Average fare of all the journey

--- src/com/bridgelabz/cab_service/FareCalculator.java
+++ src/com/bridgelabz/cab_service/FareCalculator.java
@@ -4,14 +4,17 @@ public class FareCalculator {
 	static final int COST_PER_KM = 10;
 	static final int COST_PER_MINUTE = 1;
 	static final int MINIMUM_FARE = 5;
+	static double count;
+	static double totalFareForAllJourneys;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Cab Service project");
-		//Step 1: Calculating total fare which returns the total fare
+		// Step 1: Calculating total fare which returns the total fare
 		double totalFare = totalfare(0, 4); // distance,time
 		double totalFare2 = totalfare(10, 4); // distance,time
-		System.out.println(totalFare);
-		System.out.println(totalFare2);
+
+		// Step2: Calculating the average of all the fare
+		averageOfFare();
 	}
 
 	private static double totalfare(int distance, int time) {
@@ -22,7 +25,14 @@ public class FareCalculator {
 		if (totalFare < MINIMUM_FARE) {
 			totalFare = MINIMUM_FARE;
 		}
-		System.out.println("Total fare of the journey is: " + totalFare);
+		count++;
+		System.out.println("Total fare of the journey is: " + totalFare + " and number of journey is: " + (int) count);
+		totalFareForAllJourneys += totalFare;
 		return totalFare;
+	}
+
+	private static void averageOfFare() {
+		double avg = totalFareForAllJourneys / count;
+		System.out.println("Avarage for all the journey is: " + avg);
 	}
 }
