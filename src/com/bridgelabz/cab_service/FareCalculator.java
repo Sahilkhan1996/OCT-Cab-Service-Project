da92a81ab718fff238b8816ee4ef440cb3f42cdb
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-12-20 18:48:37 +0530

    Refactor the code to get the total journey, number of journey and avg of journey after each ride

--- src/com/bridgelabz/cab_service/FareCalculator.java
+++ src/com/bridgelabz/cab_service/FareCalculator.java
@@ -11,10 +11,14 @@ public class FareCalculator {
 		System.out.println("Welcome to Cab Service project");
 		// Step 1: Calculating total fare which returns the total fare
 		double totalFare = totalfare(0, 4); // distance,time
-		double totalFare2 = totalfare(10, 4); // distance,time
+		double totalFare2 = totalfare(10, 5); // distance,time
+		double totalFare3 = totalfare(20, 6); // distance,time
+		double totalFare4 = totalfare(30, 7); // distance,time
+		double totalFare5 = totalfare(40, 8); // distance,time
 
 		// Step2: Calculating the average of all the fare
-		averageOfFare();
+		double avg1 = averageOfFare();
+		System.out.println("Average for all the journey is: " + avg1);
 	}
 
 	private static double totalfare(int distance, int time) {
@@ -26,13 +30,17 @@ public class FareCalculator {
 			totalFare = MINIMUM_FARE;
 		}
 		count++;
-		System.out.println("Total fare of the journey is: " + totalFare + " and number of journey is: " + (int) count);
 		totalFareForAllJourneys += totalFare;
+		// Step 3: Calculating the total fare number of journey and average of all the
+		// journey
+		System.out.println("Total fare of the journey: " + totalFare + " and number of journey: " + (int) count + ""
+				+ " Average for all the journey: " + averageOfFare());
 		return totalFare;
 	}
 
-	private static void averageOfFare() {
+	private static double averageOfFare() {
 		double avg = totalFareForAllJourneys / count;
-		System.out.println("Avarage for all the journey is: " + avg);
+		// System.out.println("Average for all the journey is: " + avg);
+		return avg;
 	}
 }
